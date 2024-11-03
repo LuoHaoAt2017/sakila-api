5fd9fbacfef578321b40ca1b28c8ffca9cdb93e3
LuoHao <hluo5206@gmail.com>
2024-11-03 19:45:13 +0800

    拦截器

---
+++ src/interceptors/logging.interceptor.ts
@@ -0,0 +1,21 @@
+import {
+  Injectable,
+  NestInterceptor,
+  ExecutionContext,
+  CallHandler,
+} from '@nestjs/common';
+import { Observable } from 'rxjs';
+import { tap } from 'rxjs/operators';
+/**
+ * 拦截器可以是控制器作用域的、方法作用域的或全局作用域的。
+ */
+@Injectable()
+export class LoggingInterceptor implements NestInterceptor {
+  intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
+    console.log('Before...');
+    const now = Date.now();
+    return next
+      .handle()
+      .pipe(tap(() => console.log(`After... ${Date.now() - now}ms`)));
+  }
+}
--- src/modules/product/product.controller.ts
+++ src/modules/product/product.controller.ts
@@ -1,13 +1,27 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  Post,
+  UseInterceptors,
+} from '@nestjs/common';
 import { ProductService } from './product.service';
 import { Product } from './product.entity';
 import { PageDto } from 'src/dtos/PageDto';
+import { LoggingInterceptor } from 'src/interceptors/logging.interceptor';
 
+@UseInterceptors(LoggingInterceptor) // 我们传递了 LoggingInterceptor 类（而不是实例），将实例化的责任留给框架并启用依赖注入。
 @Controller({
   path: '/product',
 })
 export class ProductController {
   constructor(private readonly productService: ProductService) {}
+
+  @Get('/detail')
+  getProductById(@Param() id: number) {
+    return this.productService.findOne(id);
+  }
 
   @Post('/page')
   getProductByPage(@Body() paging: PageDto): Promise<Product[]> {
