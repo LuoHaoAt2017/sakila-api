47b6e86c19d05066b9b36c19f403556f802dc0b7
LuoHao <hluo5206@gmail.com>
2024-11-24 10:52:39 +0800

    com.imooc.sakila

--- src/main/java/com/imooc/sakila/model/condition/ProductCondition.java
+++
@@ -1,8 +0,0 @@
-package com.imooc.springboot.model.condition;
-import com.imooc.springboot.model.PageParam;
-import lombok.Data;
-
-@Data
-public class ProductCondition extends PageParam {
-    private String prodName;
-}
--- src/test/java/com/imooc/springboot/service/Waiter.java
+++
@@ -1,6 +0,0 @@
-package com.imooc.springboot.service;
-
-public interface Waiter {
-    void greetTo(String clientName);
-    void serveTo(String clientName);
-}
